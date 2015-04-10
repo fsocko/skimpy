5b2ef24e134ab41bdf926a7de428ee58765282b6
fsocko <fsocko@gmail.com>
2015-04-10 02:17:41 +0100

    Status messages to SpiderToDB so we know it's not crashed.

--- src/BusinessLogic/SpiderToDB.java
+++ src/BusinessLogic/SpiderToDB.java
@@ -21,7 +21,8 @@ public class SpiderToDB extends HttpServlet{
 
 		 public int countLines(String inputFile) //Most of this method written by a very helpful chap called Yashwant Chavan.  
 		 {
-			  int lines = 0;
+			 System.out.println("Counting the number of lines in the file: " + inputFile + "..."); 
+			 int lines = 0;
 			  try {
 			   File file = new File(inputFile);
 			   LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(file));
@@ -35,7 +36,7 @@ public class SpiderToDB extends HttpServlet{
 			  } catch (IOException e) {
 			   System.out.println("IOException Occured" + e.getMessage());
 			  }
-	
+			  System.out.print("Done");
 			  return lines;
 
 		 }
@@ -85,7 +86,8 @@ public class SpiderToDB extends HttpServlet{
 	 //Takes about 2min, but it beats the 20mins or so in a sequential search.
 	 public ArrayList readAllRecords(String file)
 	    { 
-			 
+			System.out.println("Adding all lines of the file: " + file + "to an ArrayList.");
+			System.out.println("This usually takes 1-3 minutes...");
 		 	ArrayList allRec = new ArrayList();
 			 
 			 FileInputStream fs = null;
@@ -121,7 +123,9 @@ public class SpiderToDB extends HttpServlet{
 			    	 { System.out.println("IOException while closing.");}
 			     }	     	     
 			 }
-			 return allRec;	 
+			 System.out.println("Done");
+			 return allRec;
+			 
 	    }
 	 
 	 
