b15deab44eed2ea837928df5032138fd723ccc5a
LeeStuart <leestuart.m@gmail.com>
2015-04-12 18:53:59 +0100

    Command line interface for Scraper done

---
+++ src/ProductExtractor/ScraperMainInterface.java
@@ -0,0 +1,38 @@
+package ProductExtractor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ScraperMainInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			InputStreamReader cin = new InputStreamReader(System.in);
+			BufferedReader br = new BufferedReader(cin);
+			System.out.println("Which website would you like to scrape?");
+			System.out.println("[t = tesco, s = sainsburys]");
+			char userInput = br.readLine().charAt(0);
+			switch (userInput) {
+			case 't':
+				TescoScraper.main(new String[0]);
+				break;
+			case 's':
+				System.out.println("How many threads would you like to run?");
+				System.out.println("Default is 15");
+				int noThreads;
+				try {
+					noThreads = Integer.parseInt(br.readLine().trim());
+				} catch (Exception e) {
+					noThreads = 15;
+				}
+				SainsburysScraper.runScraper(noThreads, true);
+				break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
