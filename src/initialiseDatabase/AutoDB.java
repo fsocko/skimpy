f9c32d5cba62ae924678975fb690e1a78f8fb815
fsocko <fsocko@gmail.com>
2015-04-27 23:38:03 +0100

    added regular pushToDD(char) functionality, rather than menu.

--- src/initialiseDatabase/AutoDB.java
+++ src/initialiseDatabase/AutoDB.java
@@ -1,12 +1,19 @@
 //@Author: FPS
 package initialiseDatabase;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.Statement;
+import java.util.ArrayList;
+
+import BusinessLogic.DBConnect;
+import BusinessLogic.Food;
+import BusinessLogic.SpiderToDB;
 
 import com.mysql.jdbc.*;
 
@@ -15,130 +22,127 @@ import com.mysql.jdbc.*;
 //Some code modified from http://www.tutorialspoint.com/jdbc
 public class AutoDB {
 
-	//initDB();// initialise DB using contents from default init file.
-	//initTable('a'); initialise Asda table.
-	//initTable('s'); sainsbury's
-	//initTable('t'); tesco
-	//initTable('u'); user_info
+    // initDB();// initialise DB using contents from default init file.
+    // initTable('a'); initialise Asda table.
+    // initTable('s'); sainsbury's
+    // initTable('t'); tesco
+    // initTable('u'); user_info
 
     // JDBC driver name and database URL
     static final String localHost = "jdbc:mysql://localhost/";
     // Database credentials
     static final String user = "root";
     static final String pass = "";
+    
+    // This array stores names of tables.
+    static final String[] tableNames = { "asda", "sains", "tesco", "user_info" };
 
-    //initialise a table in skimpy database.
-    //a for ASDA
-    //s for Sainsbury's
-    //t for Tesco
-    //u for user_info
-    public void initTable(char dTable)
-    {
-		//these Arrays contain the filepath of SQL files used as 
-		//templates for initialising the tables. they produce
-		//empty, initialised tables when run.
-		final String[] SQLFiles = {"SQLFiles/initDB/asdaInit.sql", "SQLFiles/initDB/sainsInit.sql",
-					   "SQLFiles/initDB/tescoInit.sql", "SQLFiles/initDB/userInit.sql"};
-		
-		//This array stores names of tables. 
-		final String[] tableNames = {"asda", "sains", "tesco", "user_info"};
-		
-		String SQLPath = null; //filepath of input SQL file
-		String tableName = null; //name of table to drop and initialise
-	
-		switch(dTable)
-		{
-    		case 'a':
-    		SQLPath = SQLFiles[0];
-    		tableName = tableNames[0];
-    		break;
-    		
-    		case 's':
-    		SQLPath = SQLFiles[1];
-    		tableName = tableNames[1];    
-    		break;
-    		
-    		case 't':
-    		SQLPath = SQLFiles[2];
-    		tableName = tableNames[2];
-    		break;
-    		
-    		case 'u':
-    		SQLPath = SQLFiles[3];
-    		tableName = tableNames[3];
-    		break;
-    		
-    		default:
-    		System.out.println("Invalid input.");
-    		System.exit(0);
-    		break;
+    // initialise a table in skimpy database.
+    // a for ASDA
+    // s for Sainsbury's
+    // t for Tesco
+    // u for user_info
+    public void initTable(char dTable) {
+	// these Arrays contain the filepath of SQL files used as
+	// templates for initialising the tables. they produce
+	// empty, initialised tables when run.
+	final String[] SQLFiles = { "SQLFiles/initDB/asdaInit.sql",
+		"SQLFiles/initDB/sainsInit.sql",
+		"SQLFiles/initDB/tescoInit.sql", "SQLFiles/initDB/userInit.sql" };
+
+	String SQLPath = null; // filepath of input SQL file
+	String tableName = null; // name of table to drop and initialise
+
+	switch (dTable) {
+	case 'a':
+	    SQLPath = SQLFiles[0];
+	    tableName = tableNames[0];
+	    break;
+
+	case 's':
+	    SQLPath = SQLFiles[1];
+	    tableName = tableNames[1];
+	    break;
+
+	case 't':
+	    SQLPath = SQLFiles[2];
+	    tableName = tableNames[2];
+	    break;
+
+	case 'u':
+	    SQLPath = SQLFiles[3];
+	    tableName = tableNames[3];
+	    break;
+
+	default:
+	    System.out.println("Invalid input.");
+	    System.exit(0);
+	    break;
+	}
+
+	Connection conn = null;
+	Statement stmt = null;
+
+	try {
+	    // Register JDBC driver
+	    Class.forName("com.mysql.jdbc.Driver");
+
+	    // Connect to skimpy DB
+	    System.out.print("Selecting database...");
+	    conn = null;
+
+	    conn = DriverManager.getConnection("jdbc:mysql://localhost/skimpy",
+		    user, pass);
+	    System.out.print("Done\n");
+
+	    // Execute a query - Drop Table
+	    stmt = conn.createStatement();
+
+	    String dropT = "DROP TABLE IF EXISTS " + tableName + ";";
+	    System.out.print("Dropping " + tableName + " if it exists...");
+	    stmt.executeUpdate(dropT);
+	    System.out.print("Done\n");
+
+	    // initialise selected table from SQL
+	    System.out.print("Creating table, " + tableName
+		    + " as defined in \"" + SQLPath + "\":  ...");
+
+	    // Setup scriptRunner class to add all tables as read from an
+	    // SQL template
+
+	    File file = new File(SQLPath);
+	    FileReader fr = new FileReader(file);
+	    AutoDBScriptRunner sr = new AutoDBScriptRunner(conn, false, true);
+	    sr.runScript(fr);
+	    System.out.print("\nDone. Database initialised with no errors.\n");
+
+	} catch (SQLException se) {
+	    System.out
+		    .println("Sorry, there is a problem with the SQL server.\n\n");
+	    se.printStackTrace();
+	} catch (Exception e) {
+	    System.out.println("Sorry, the following exception occurred:\n\n");
+	    e.printStackTrace();
+	} finally {
+	    if (conn != null) {
+		System.out.print("Closing original Connection to " + localHost
+			+ "...");
+		try {
+		    conn.close();
+		} catch (SQLException e) {
+		    e.printStackTrace();
 		}
-	
-	       Connection conn = null;
-	       Statement stmt = null;   
-		   
-		try{
-		    //Register JDBC driver
-		      Class.forName("com.mysql.jdbc.Driver");
+		System.out.print("Done\n");
 
-		      //Connect to skimpy DB
-			 System.out.print("Selecting database...");
-			 conn = null;
-			
-			     conn = DriverManager.getConnection("jdbc:mysql://localhost/skimpy", user, pass);
-			     System.out.print("Done\n");
+	    }
+	}
+    }
 
-            		     //Execute a query - Drop Table
-            		     stmt = conn.createStatement();
-            		      
-            		     String dropT = "DROP TABLE IF EXISTS " +tableName +";";
-            		     System.out.print("Dropping " +tableName+ " if it exists...");
-            		     stmt.executeUpdate(dropT);
-            		     System.out.print("Done\n");
-            		      
-            		     //initialise selected table from SQL
-            		     System.out.print("Creating table, " + tableName+" as defined in \""+SQLPath+"\":  ...");
-            		     
-            		     // Setup scriptRunner class to add all tables as read from an
-            		     // SQL template
-
-            			File file = new File(SQLPath); 				      
-            			FileReader fr = new FileReader(file);
-            			AutoDBScriptRunner sr = new AutoDBScriptRunner(conn, false,
-            				true);
-            			sr.runScript(fr);
-            			System.out.print("\nDone. Database initialised with no errors.\n");
-    
-			}
-			 catch (SQLException se) {
-				    System.out.println("Sorry, there is a problem with the SQL server.\n\n");
-				    se.printStackTrace();
-				} catch (Exception e) {
-				    System.out.println("Sorry, the following exception occurred:\n\n");
-				    e.printStackTrace();
-				} 
-			 finally {
-					if (conn != null)
-					{   
-					    System.out.print("Closing original Connection to " + localHost + "...");
-					    try {
-						conn.close();
-					    } catch (SQLException e) {
-						e.printStackTrace();
-					    }
-					    System.out.print("Done\n");	
-					   
-					}
-			 	}		
-			 }    
-    
-    
-    //initialise Skimpy DB with default SQL file
-    public void initDB()
-    {
+    // initialise Skimpy DB with default SQL file
+    public void initDB() {
 	initDB("skimpy", "SQLFiles/initDB/fullDB.sql");
     }
-    
+
     // initialise Skimpy DB with selected SQL file.
     public void initDB(String SQLfilePath) {
 	// Initialise as skimpy by default. pass a String with a different
@@ -204,14 +208,15 @@ public class AutoDB {
 		// SQL template
 		System.out.print("Adding tables as defined in " + SQLPath
 			+ "...\n\n");
-		File file = new File(SQLPath); 				      
+		File file = new File(SQLPath);
 		// if the default SQL file is not in data
 		file.createNewFile();
 		FileReader fr = new FileReader(file);
 		AutoDBScriptRunner sr = new AutoDBScriptRunner(conn, false,
 			true);
 		sr.runScript(fr);
-		System.out.print("\nDone. Database initialised with no errors.\n");
+		System.out
+			.print("\nDone. Database initialised with no errors.\n");
 
 	    } catch (SQLException se) {
 		se.printStackTrace();
@@ -233,4 +238,55 @@ public class AutoDB {
 	    }
 	}
     }
+
+    // Menu for setting up pushing to database
+    public static void pushToDB(char sm) 
+    {
+	 String tescoPath = "data/tesco.txt";
+	 String sainsPath = "data/sains.txt";
+	 String asdaPath = "data/asda.txt";
+
+		switch (sm) 
+		{
+		case 'a':
+		    System.out.print("Pushing ASDA data...");
+		    pushToDB(asdaPath, "asda");
+		    System.out.print("Done\n");
+		    break;
+		case 's':
+		    System.out.print("Pushing Sainsbury's data...");
+		    pushToDB(sainsPath, "sains");
+		    System.out.print("Done\n");
+		    break;
+		case 't':
+		    System.out.print("Pushing Tesco data...");
+		    pushToDB(tescoPath, "tesco");
+		    System.out.print("Done\n");
+		    break;
+		default:
+		    System.out.println("Invalid input.\n");
+		    break;
+
+		}
+	    }
+  
+    	//parse data from web scrapers and push data to database
+	public static void pushToDB(String path, String table)
+	{
+		SpiderToDB std = new SpiderToDB();
+		DBConnect toDB = new DBConnect();
+		ArrayList Items = new ArrayList(std.readAllRecords(path));
+		
+		int i = 1;
+		int t = std.countLines(path);
+		while(i < t){
+			Food foodItem = std.formatRecord(path, Items.get(i).toString().trim());
+			if(foodItem != null)
+			{
+				System.out.println(foodItem.toString());
+				toDB.pushFood(foodItem, table);
+			}
+			i++;
+		}
+	}
 }
