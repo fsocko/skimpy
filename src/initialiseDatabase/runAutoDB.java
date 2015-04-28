ccdf45ad833af80bd8865e3243008133bfa35e01
fsocko <fsocko@gmail.com>
2015-04-28 04:07:38 +0100

    Running sains scraper.

--- src/initialiseDatabase/runAutoDB.java
+++ src/initialiseDatabase/runAutoDB.java
@@ -5,15 +5,15 @@ public class runAutoDB {
     public static void main(String[] args)
     {
 	AutoDB atb = new AutoDB();
-	atb.initDB();// initialise DB using contents from default init file.
-	atb.initTable('a'); //initialise Asda table.
+	//atb.initDB();// initialise DB using contents from default init file.
+	//atb.initTable('a'); //initialise Asda table.
 	atb.initTable('s'); //sainsbury's
-	atb.initTable('t'); //tesco
-	atb.initTable('u'); //user_info
+	//atb.initTable('t'); //tesco
+	//atb.initTable('u'); //user_info
 
-	atb.pushToDB('a'); //push asda scraper data
+	//atb.pushToDB('a'); //push asda scraper data
 	atb.pushToDB('s'); //push sains scraper data
-	atb.pushToDB('t'); //push Tesco scraper data
+	//atb.pushToDB('t'); //push Tesco scraper data
     }
     
 }
