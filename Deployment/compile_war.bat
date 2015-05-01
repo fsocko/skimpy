fe25f352fa4af5763a3ad084f08114f6124ba704
fsocko <fsocko@gmail.com>
2015-05-01 02:50:55 +0100

    Removed Extra Databases, extended name in initDB

--- Deployment/compile_war.bat
+++ Deployment/compile_war.bat
@@ -2,5 +2,5 @@ cd ..
 mkdir WebContent\WEB-INF\classes\
 javac -d "WebContent\WEB-INF\classes" -classpath "library\*;WebContent\WEB-INF\classes" src\BusinessLogic\*.java src\initialiseDatabase\*.java src\ProductExtractor\*.java
 cd WebContent
-jar cvf ..\Deployment\skimpy.war .
+jar cvf ..\Deployment\Skimpy.war .
 cd Deployment
